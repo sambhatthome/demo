3a0e33e50ec87bc25abe66e0e1781950a86a9419
bhatt72 <bhatt72@purdue.edu>
2026-07-14 13:11:51 -0700

    add pause resume endpoints

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import TodoItem
 
-# Create your views here.
-
 def home(request):
     return render(request, "home.html")
 
@@ -10,7 +8,8 @@ def todos(request):
     items = TodoItem.objects.all()
     return render(request, "todos.html", {"todos": items})
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication
 from myapp.models import IMURecord
 from myapp.serializers import IMURecordSerializer
@@ -19,13 +18,36 @@ class CsrfExemptSessionAuthentication(SessionAuthentication):
     def enforce_csrf(self, request):
         return
 
+collection_paused = False
+
 class IMURecordViewSet(viewsets.ModelViewSet):
     queryset = IMURecord.objects.all().order_by('timestamp')
     serializer_class = IMURecordSerializer
     authentication_classes = (CsrfExemptSessionAuthentication,)
 
+    def create(self, request, *args, **kwargs):
+        if collection_paused:
+            return Response({'status': 'paused, data ignored'}, status=status.HTTP_200_OK)
+        return super().create(request, *args, **kwargs)
+
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import time
 
 def current_time(request):
     return JsonResponse({'epoch': int(time.time())})
+
+def collection_status(request):
+    return JsonResponse({'paused': collection_paused})
+
+@csrf_exempt
+def pause_collection(request):
+    global collection_paused
+    collection_paused = True
+    return JsonResponse({'status': 'paused'})
+
+@csrf_exempt
+def resume_collection(request):
+    global collection_paused
+    collection_paused = False
+    return JsonResponse({'status': 'resumed'})
